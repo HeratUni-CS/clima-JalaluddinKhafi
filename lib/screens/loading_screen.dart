cbb2a43708a1672ecc7fcbdcb179ed30be81191a
Jalaluddin Khafi <Khafi98117@gmail.com>
2023-06-06 16:25:35 +0430

    seperate location file created with properties

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:geolocator_android/geolocator_android.dart';
+import 'package:clima/services/location.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -16,13 +16,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
 
   void getPosition() async {
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } catch (e) {
-      print(e);
-    }
+    location locations=location();
+    await locations.getCurrentLocation();
+    print(locations.latitude);
+    print(locations.longitude);
+
   }
 
   @override
