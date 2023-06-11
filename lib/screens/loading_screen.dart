886e55384ceea7659b0bfc91a34049632c0cd34d
Jalaluddin Khafi <Khafi98117@gmail.com>
2023-06-11 22:45:19 +0430

    combine getData method with getLocation method

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-
+import 'package:clima/services/networking.dart';
 
 const apiKey='4c5b86cf1ab33e8139ecd16a4bd5b30e';
 class LoadingScreen extends StatefulWidget {
@@ -24,22 +24,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await locations.getCurrentLocation();
     latitude=locations.latitude;
     longitude=locations.longitude;
-    getData();
-  }
-  void getData() async {
-    var uri=Uri.parse("https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey");
-    http.Response response= await http.get(uri);
-    String data=response.body;
+    var uri=Uri.parse(    "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey");
+    NetworkHelper networkHelper =NetworkHelper(uri);
 
-      double temperature=decodedData['main']['temp'];
-      int condition =decodedData['weather'][0]['id'];
-      String cityName=decodedData['name'];
-
-     print(temperature);
-     print(condition);
-     print(cityName);
+    double temperature=decodedData['main']['temp'];
+    int condition =decodedData['weather'][0]['id'];
+    String cityName=decodedData['name'];
 
   }
+
 
   @override
   Widget build(BuildContext context) {
